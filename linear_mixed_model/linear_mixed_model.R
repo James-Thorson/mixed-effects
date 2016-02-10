53b9cab1d82f617df047d1fafcb3c5633fc1142c
Jim Thorson <JamesT.esq@gmail.com>
2016-02-10 13:00:45 -0800

    simplified linear_mixed_model script

--- linear_mixed_model/linear_mixed_model.R
+++ linear_mixed_model/linear_mixed_model.R
@@ -1,11 +1,12 @@
 
 setwd( "C:/Users/James.Thorson/Desktop/Project_git/mixed-effects/linear_mixed_model" )
+Use_REML = TRUE
 
 ######################
 # Simulate data
 ######################
 
-Factor = rep( 1:1000, each=1000)
+Factor = rep( 1:10, each=10)
 Z = rnorm( length(unique(Factor)), mean=0, sd=1)
 
 X0 = 0
@@ -18,7 +19,7 @@ Y = Z[Factor] + X0 + rnorm( length(Factor), mean=0, sd=1)
 
 library(lme4)
 
-Lme = lmer( Y ~ 1|factor(Factor))
+Lme = lmer( Y ~ 1|factor(Factor), REML=Use_REML)
 
 ######################
 # Run in TMB
@@ -26,12 +27,13 @@ Lme = lmer( Y ~ 1|factor(Factor))
 
 library(TMB)
 
-Version = c( "linear_mixed_model", "linear_mixed_model_parallel")[2]
+Version = "linear_mixed_model"
 compile( paste0(Version,".cpp") )
 
 Data = list( "n_data"=length(Y), "n_factors"=length(unique(Factor)), "Factor"=Factor-1, "Y"=Y)
 Parameters = list( "X0"=-10, "log_SD0"=2, "log_SDZ"=2, "Z"=rep(0,Data$n_factor) )
 Random = c("Z")
+if( Use_REML==TRUE ) Random = union( Random, "X0")
 
 dyn.load( dynlib("linear_mixed_model") )
 Obj = MakeADFun(data=Data, parameters=Parameters, random=Random)
@@ -48,14 +50,6 @@ Opt = nlminb( start=Obj$par, objective=Obj$fn, gradient=Obj$gr, control=list("tr
   Report = Obj$report()
   SD = sdreport( Obj, bias.correct=TRUE )
 
-# Bias correct experiment
-SD$unbiased$value['SampleVarZ']
-Report$SampleVarZ  
-SD$unbiased$value['SampleSDZ']
-Report$SampleSDZ
-SD$unbiased$value['SDZ']
-Report$SDZ  
-
 ######################
 # Compare estimates
 ######################
@@ -69,16 +63,4 @@ cbind( "True"=Z, ranef(Lme)[['factor(Factor)']], Report$Z )
 # Variances
 summary(Lme)
 unlist( Report[c("SDZ","SD0")] )
-
-#########################
-# Experiment with parallelization
-#########################
-
-# Parallelized experimentations
-ben <- benchmark(Obj, n=100, cores=1:4)
-plot(ben)
-
-# Parallel
-ben <- benchmark(Obj, n=1, cores=1:2, expr=expression(do.call("nlminb",obj)))
-plot(ben)
 
